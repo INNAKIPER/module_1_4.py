f41607e70f98b5a72722ec3c7fda1d20ecfb8fbe
INNAKIPER <kiperinna4@gmail.com>
2024-10-07 08:19:28 +0300

    Практическая работа по уроку "Организация программ и методы строк

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+from nt import replace
+
+my_string=input()
+print(my_string.upper())
+print(my_string.lower())
+mystring=input()
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
